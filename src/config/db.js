c460d357428f586ba280a339915f9fbb032f4431
Erick <erick@utm.edu.ec>
2026-08-07 18:07:50 -0500

    Fix definitivo conexion BD

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,8 @@
 const { Pool } = require('pg');
 
-let pool;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
-function getPool() {
-  if (!pool) {
-    pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    });
-  }
-  return pool;
-}
-
-module.exports = { query: (...args) => getPool().query(...args) };
+module.exports = pool;
